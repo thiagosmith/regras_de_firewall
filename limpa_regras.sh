c00f2fc96e76f03489dc93c4654208d51f454e8d
Smith Braz <106747435+thiagosmith@users.noreply.github.com>
2026-01-20 21:14:00 -0300

    Create limpa_regras.sh

---
+++ limpa_regras.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Desativar forward
+sysctl -w net.ipv4.ip_forward=0
+
+# Zerar log iptables
+echo -n > /var/log/iptables.log
+
+# Limpar todas regras
+iptables -F
+
+# Removendo Chains
+iptables -X
+
+# Zerar contador
+iptables -Z
+
+# Libera trafego para todas poli負icas
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
